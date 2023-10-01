bcb57015b2c4f6c0852f25306ee11bd29b7d673e
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-02 01:09:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import react router dom
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+// import pages 
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+// import component 
+
+import Sidebar from './components/Sidebar';
+
+
+
+const App = () => {
+  return <div className='overflow-hidden'>react app</div>;
+};
+
+export default App;
